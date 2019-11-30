1bc4683219f3511763fdb47da831b323999b1c6d
Alek Hadzidedic <alek.hadzidedic@gmail.com>
2019-11-30 03:08:08 -0500

    added user options page with preliminary logic, added more endpoints

--- app.py
+++ app.py
@@ -1,7 +1,13 @@
 from flask import Flask, render_template, request
+from models.user import User
 
 # app = Flask(__name__)
 app = Flask(__name__, static_folder='./static')
+
+user = User('test', 'er', 'ere@ere.com')
+user.user_type = 'Instructor'
+teams = []
+are_team_parameters_set = False
 
 
 @app.route('/')
@@ -19,9 +25,22 @@ def register():
     return render_template("register.html")
 
 
-@app.route('/create-team')
-def create_team():
+@app.route('/user/options')
+def user_options():
+    return render_template("user-options.html", user=user)
+
+
+@app.route('/team')
+def team_options():
     return render_template("create-team.html")
+
+
+@app.route('/team/parameters')
+def team_parameters():
+    if user.user_type == 'Instructor':
+        return render_template("set-team-parameters.html")
+    else:
+        return render_template("index.html")
 
 
 if __name__ == '__main__':
