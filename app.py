fa0c3214b4d2f93d8a4ad3838ce8aef77e9ee534
ebropower <ebropower@gmail.com>
2019-12-01 09:51:31 -0500

    registration working nicely. login/logout working too. may need to fix minor bugs

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, request, session, redirect, url_for, g
 from db.DatabaseConnection import DatabaseConnection
 from db.config import config
 import os
+from werkzeug.security import generate_password_hash, check_password_hash
 
 # app = Flask(__name__)
 app = Flask(__name__, static_folder='./static')
@@ -19,13 +20,84 @@ def index():
 def sign_in():
     if request.method == 'POST':
         session.pop('user', None) # clear session
-        if request.form['password'] == 'password': # compare hash pass here instead
 
-            ### NEED TO CHECK IF STUDENT OR INSTRUCTOR, and assign that to session variable  *******
+        email = request.form['email']
+        password = request.form['password']
 
-            session['user'] = request.form['email']
+        try:
+            db = DatabaseConnection()
 
-            return redirect(url_for('dashboard'))
+            with db.get_connection() as con:
+
+                cursor = con.cursor()
+                cursor.execute("SELECT * FROM tms.student WHERE tms.student.email = (%s)",(email,))
+                dbEmailResultStudent = cursor.fetchone()
+
+                print("TYPE OF OBJECT: " + str(type(dbEmailResultStudent)))
+                print ("STUDENT DATA (NOT LOGGED YET): " + str(dbEmailResultStudent)) # STUDENT'S DATA
+
+                if (dbEmailResultStudent != None): # Student with email found
+                    
+                    print("THIS SHOULDNT PRINT")
+                    dbHashedPass = dbEmailResultStudent[5]
+
+                    userType = "student"
+
+                    print ("HASHED STUDENT PASS (NOT LOGGED YET): " + str(dbHashedPass)) # THE HASHED PASSWORD FROM DB FOR THAT USER
+
+                else: # Check if instructor
+
+                    print("THIS SHOULD PRINT")
+                    cursor = con.cursor()
+                    cursor.execute("SELECT * FROM tms.instructor WHERE tms.instructor.email = (%s)",(email,))
+                    dbEmailResultInstructor = cursor.fetchone()
+
+                    if (dbEmailResultInstructor != None): # instructor with email found
+                        print("THIS SHOULD ALSO PRINT")
+    
+                        dbHashedPass = dbEmailResultInstructor[4]
+                        
+                        print("THIS MUST ALSO PRINT")
+
+                        userType = "instructor"
+
+                        print ("HASHED INSTRUCTOR PASS (NOT LOGGED YET): " + str(dbHashedPass))
+
+                    else:
+                        flash("No user with such email exists.")
+                        return redirect(url_for("sign_in"))
+
+                if (check_password_hash(dbHashedPass, password)): # compare hash pass here instead
+
+                    ### NEED TO CHECK IF STUDENT OR INSTRUCTOR, and assign that to session variable  *******
+                    session['user'] = request.form['email']
+                    session['userType'] = userType
+                    
+                    if (userType == "student"):
+                        session['firstName'] = dbEmailResultStudent[1]
+                        session['lastName'] = dbEmailResultStudent[2]
+                        session['studentNumber'] = dbEmailResultStudent[3]
+                        session['isLiaison'] = dbEmailResultStudent[6]
+                        session["teamName"] = dbEmailResultStudent[7]
+                    else:
+                        session['firstName'] = dbEmailResultInstructor[1]
+                        session['lastName'] = dbEmailResultInstructor[2]
+
+                    flash("Login Successful")
+                    return redirect(url_for('dashboard'))
+
+                else:
+                    flash("Invalid Password")
+                    return redirect(url_for("sign_in"))
+
+        except Exception as err:
+            con.rollback()
+            return f"{err.__class__.__name__}: {err}"
+            # flash("Error in registering user")
+        
+        finally:
+            con.close()
+            #return render_template("register.html")
         
     if g.user: # if already signed in, then redirect user to dashboard
         return redirect(url_for('dashboard'))
@@ -44,8 +116,26 @@ def sign_out():
 @app.before_request
 def before_request():
     g.user = None
+    g.user_type = None
+    g.user_first_name = None
+    g.user_last_name = None
+    g.student_number = None
+    g.student_is_liaison = None
+    g.student_team_name = None
+
     if 'user' in session:
         g.user = session['user']
+        g.user_type = session['userType']
+        g.user_first_name = session['firstName']
+        g.user_last_name = session['lastName']
+
+    if g.user_type == "student":
+        g.student_number = session['studentNumber']
+        g.student_is_liaison = session['isLiaison']
+        g.student_team_name = session["teamName"]
+
+    potato = ("EMAIL: " + str(g.user), "USER TYPE: " + str(g.user_type), "FIRSTNAME: " + str(g.user_first_name), "LAST NAME: " + str(g.user_last_name), "STUDENT NUM: " + str(g.student_number), "IS_LIAISON: " + str(g.student_is_liaison), "TEAM NAME: " + str(g.student_team_name))
+    print("LOGGED IN: " + str(potato))
 
 @app.route('/getsession')
 def getsession():
@@ -60,24 +150,33 @@ def dropsession():
     
     return "Dropped"
 
-
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     if request.method == 'POST':
         session.pop('user', None) # clear session
         
         try:
-            firstname = request.form['firstname']
-            lastname = request.form['lastname']
+            firstname = request.form['first_name']
+            lastname = request.form['last_name']
             email = request.form['email']
             password = request.form['password']
+            userType = request.form['user_type']
          
+            hashedPass = generate_password_hash(password)
+
             db = DatabaseConnection()
 
             with db.get_connection() as con:
 
                 cursor = con.cursor()
-                cursor.execute("INSERT INTO tms.student (first_name,last_name,email,password, team_name) VALUES (%s, %s, %s, %s, NULL)",(firstname,lastname,email,password))
+                
+                if (userType == "student"): #if it's a student
+                    
+                    studentNumber = request.form['student_number']
+
+                    cursor.execute("INSERT INTO tms.student (first_name, last_name, student_number, email, password, team_name) VALUES (%s, %s, %s, %s, %s, NULL)",(firstname, lastname, studentNumber, email, hashedPass))
+                else: #it's an instructor
+                    cursor.execute("INSERT INTO tms.instructor (first_name, last_name, email, password) VALUES (%s, %s, %s, %s)",(firstname, lastname, email, hashedPass))
 
                 flash("Successfully registered user")
   
@@ -89,7 +188,6 @@ def register():
         finally:
             con.close()
             #return render_template("register.html")
-
 
     if g.user: # if logged in, redirect to dashboard
         return redirect(url_for("dashboard"))
@@ -112,6 +210,7 @@ def create_team():
         return render_template("create-team.html")
     
     return redirect(url_for('sign_in'))
+    
 
 if __name__ == '__main__':
     app.run(debug=True)
--- db/sql/schemas.sql
+++ db/sql/schemas.sql
@@ -6,12 +6,12 @@ CREATE TABLE IF NOT EXISTS tms.instructor (
   first_name        VARCHAR (20) NOT NULL,
   last_name         VARCHAR (50) NOT NULL,
   email             VARCHAR (50) NOT NULL UNIQUE,
-  password          TEXT
+  password          VARCHAR (255)
 );
 
 CREATE TABLE IF NOT EXISTS tms.team (
   id                SERIAL PRIMARY KEY,
-  team_name              VARCHAR (50) NOT NULL UNIQUE,
+  team_name         VARCHAR (50) NOT NULL UNIQUE,
   max_team_size     INTEGER,
   team_size         INTEGER
 );
@@ -20,9 +20,9 @@ CREATE TABLE IF NOT EXISTS tms.student (
   id                SERIAL PRIMARY KEY,
   first_name        VARCHAR (20) NOT NULL,
   last_name         VARCHAR (50) NOT NULL,
-  student_number    INTEGER,
+  student_number    INTEGER NOT NULL UNIQUE,
   email             VARCHAR (50) NOT NULL UNIQUE,
-  password          TEXT,
+  password          VARCHAR (255),
   is_liason         boolean,
   team_name VARCHAR (50) REFERENCES tms.team (team_name) ON DELETE RESTRICT
 );
